c59002c6a96b9729f34c64d3daf72f416b127f90
Sathvika <sxp190111@utdallas.com>
2023-11-10 13:01:30 -0600

    first page

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+
+  username: string = "";
+  password: string = "";
+
+  constructor(private router: Router) { }
+
+
+  submitted = false;
+  onSubmit() { this.submitted = true; 
+    this.router.navigate(['login'])
+  }
+
+}
